5963cd07f1cee45b986416536ae0f6c94c691ebb
CodingMasterAhan <84573167+AhanPande1078@users.noreply.github.com>
2021-09-04 02:20:06 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,16 +2,19 @@ function cake() {
     document.getElementById("cake").style.display="inline-block";
     document.getElementById("eat").style.display="inline-block";
     console.log("worked");
+    window.alert("cake delivered");
 }
 function food() {
     document.getElementById("food").style.display="inline-block";
     document.getElementById("eat").style.display="inline-block";
     console.log("worked");
+    window.alert("food delivered");
 }
 function drink() {
     document.getElementById("drink").style.display="inline-block";
     document.getElementById("eat").style.display="inline-block";
     console.log("worked");
+    window.alert("drinks delivered");
 }
 function eat() {
     document.getElementById("cake").style.display="none";
@@ -19,4 +22,5 @@ function eat() {
     document.getElementById("drink").style.display="none";
     document.getElementById("eat").style.display="none";
     console.log("worked");
-}
+    window.alert("yumm");
+                 }
