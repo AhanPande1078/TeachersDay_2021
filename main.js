ccbdd78306920dc6236017013cd8b93ded8cadcb
CodingMasterAhan <84573167+AhanPande1078@users.noreply.github.com>
2021-09-04 01:20:36 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function cake() {
+    document.getElementById("cake").style.display="inline-block";
+    document.getElementById("eat").style.display="inline-block";
+    console.log("worked");
+}
+function food() {
+    document.getElementById("food").style.display="inline-block";
+    document.getElementById("eat").style.display="inline-block";
+    console.log("worked");
+}
+function drink() {
+    document.getElementById("drink").style.display="inline-block";
+    document.getElementById("eat").style.display="inline-block";
+    console.log("worked");
+}
+function eat() {
+    document.getElementById("cake").style.display="none";
+    document.getElementById("food").style.display="none";
+    document.getElementById("drink").style.display="none";
+    document.getElementById("eat").style.display="none";
+    console.log("worked");
+}
